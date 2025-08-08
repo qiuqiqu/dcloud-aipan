62b516ca3db81d2f1c02c81e2f4e14b1cce45f1c
qiuqiqu <1961237983@qq.com>
2025-08-08 22:05:59 +0800

    项目初始化

---
+++ src/main/java/com/gy/dcloudaipan/DcloudAipanApplication.java
@@ -0,0 +1,13 @@
+package com.gy.dcloudaipan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DcloudAipanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DcloudAipanApplication.class, args);
+    }
+
+}
